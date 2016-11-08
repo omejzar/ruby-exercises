49dddce05e46b0033dc088f93783ae85aa9b010e
omejzar <omlezare@gmail.com>
2016-11-08 01:11:08 -0600

    Exercise 16 added

---
+++ 16.rb
@@ -0,0 +1,14 @@
+=begin
+------------------------------- Pregunta 16------------------------------------------
+Escribir un hash con el menu de un restaurant, la llave es el nombre del plato y el valor es el precio de
+este.
+1
+restaurant_menu = { "Ramen" => 3 , "Dal Makhani" => 4 , "Coffee" => 2 }
+1. Obtener el plato mas caro
+2. Obtener el plato mas barato
+3. Sacar el promedio del valor de los platos
+4. Crear un arreglo con solo los nombres de los platos
+5. Crear un arreglo con solo los valores de los platos
+6. Modificar el hash y agregar el IVA a los valores de los platos (multiplicar por 1.19).
+7. Dar descuento del 20% para los platos que tengan un nombre de mas 1 una palabra.
+=end
