3fe403536b02980317ba9828cd6db94d9584a2d4
omejzar <omlezare@gmail.com>
2016-11-08 01:10:53 -0600

    Exercise 15 added

---
+++ 15.rb
@@ -0,0 +1,13 @@
+=begin
+------------------------------- Pregunta 15------------------------------------------
+Se tiene un arreglo de alumnos, cada alumno es un hash
+alumnos = [{ name : "Juan Cristobal" , edad : 17 }, { name : "Belen" , edad : 21 }]
+Calcular la edad promedio del curso
+Encontrar al alumno más joven del curso
+=end
+
+alumnos = [{name:"Juan Cristobal" , edad:17}, {name:"Belen" , edad:21}]
+#puts alumnos
+alumnos[edad] = gets.to_i
+#pe = alumnos[edad] / 2
+
